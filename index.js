deb9e3e40a4c99f5751439758f9eb21491c6c527
Ammar-coder87 <ammar.kolko87@gmail.com>
2023-10-08 00:57:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function() {
+    return 2000 / 100;
+}
+console.log(divide());
+
+const square = (x)=> {
+return x ** 2
+}
+console.log(square(10));
+
+const add = (y, z) => {
+return y + z;
+}
+console.log(add( 90, 10 ));
